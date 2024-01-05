9e8064edb5c14e128a2781af690467e2b8c7cf01
NiloufarSheyd <155656698+NiloufarSheid@users.noreply.github.com>
2024-01-05 19:22:06 +0330

    Add files via upload

---
+++ project-saatchi-bs/js/script.js
@@ -0,0 +1,18 @@
+class human{
+    constructor(n,a,c){
+    this.name=n,
+    this.age=a,
+    this.skinColor=c
+}
+
+}
+class niloufar extends human{
+    Programmer(){
+        console.log(`
+        programmer
+        software
+        `)
+        }
+}
+niloufar1= new niloufar("niloufar",35,"white")
+console.log(niloufar1.name)
